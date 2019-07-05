7b3cd1c8462518d83b24c8e0c3a81cbb6622a949
Bhavesh <bhaveshsingh022000@hotmail.com>
2019-07-05 16:37:55 +0530

    Added SignIn Modal

--- js/header.js
+++ js/header.js
@@ -1,14 +1,35 @@
-var mod = document.getElementById("signUpModal");
-    var bt = document.getElementById("signUp");
-    var sp = document.getElementsByClassName("close")[0];
-bt.onclick = function(){
-    mod.style.display = "block";
+var signIn = document.getElementById("signIn");
+var mod1 = document.getElementById("signInModal");
+var close1 = document.getElementsByClassName("close1")[0];
+signIn.onclick = function(){
+    mod1.style.display = "block";
 }
-sp.onclick = function(){
-    mod.style.display = "none";
+close1.onclick = function(){
+    mod1.style.display = "none";
 }
 window.onclick = function(event){
-    if(event.target == "none"){
-        mod.style.display = "none";
+    if(event.target == mod1){
+        mod1.style.display = "none";
     }
+}
+
+var signUp = document.getElementById("signUp");
+var mod2 = document.getElementById("signUpModal");
+var close2 = document.getElementsByClassName("close2")[0];
+signUp.onclick = function(){
+    mod2.style.display = "block";
+}
+close2.onclick = function(){
+    mod2.style.display = "none";
+}
+window.onclick = function(event){
+    if(event.target == mod2){
+        mod2.style.display = "none";
+    }
+}
+
+var sign = document.getElementById("member");
+sign.onclick = function(){
+    mod1.style.display = "none";
+    mod2.style.display = "block";
 }
